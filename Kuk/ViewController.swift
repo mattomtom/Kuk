d9cdcbf24276908a6ef981efccb925b8886449d8
Mateusz Tomaszewski <mateusztomaszewski@polskapress.pl>
2019-01-22 18:17:06 +0100

    + add empty project

---
+++ Kuk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kuk
+//
+//  Created by Mateusz on 1/22/19.
+//  Copyright © 2019 Mateusz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
